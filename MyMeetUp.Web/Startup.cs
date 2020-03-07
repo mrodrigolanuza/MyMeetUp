0f0a1840ea87c20ef2c52c01049ab4f2e3a301f6
Marcos Rodrigo <marcosrlanuza@hotmail.com>
2020-03-07 20:31:44 +0100

    Reorganized Startup to change behaviour depending on ASPNETCORE_ENVIRONMENT > Production or Development.

--- MyMeetUp.Web/Startup.cs
+++ MyMeetUp.Web/Startup.cs
@@ -17,47 +17,66 @@ namespace MyMeetUp.Web
 {
     public class Startup
     {
+        private readonly string _applicationName = "MyMeetUp";
         public Startup(IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+
+        //Development Configuration /////////////////////////////////////////////////////////////////
+        public void ConfigureDevelopmentServices(IServiceCollection services) {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("MyMeetUpDb_Dev")));
+            ConfigureServices(services);
+        }
+
+        public void ConfigureDevelopment(IApplicationBuilder app, IWebHostEnvironment env) {
+            app.UseDeveloperExceptionPage();
+            app.UseDatabaseErrorPage();
+            Configure(app, env);
+        }
+        
+        //Production Configuration /////////////////////////////////////////////////////////////////
+        public void ConfigureProductionServices(IServiceCollection services) {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("MyMeetUpDb_Prd")));
+            ConfigureServices(services);
+        }
+        public void ConfigureProduction(IApplicationBuilder app, IWebHostEnvironment env) {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();      // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            Configure(app, env);
+        }
+        
+        //Services /////////////////////////////////////////////////////////////////////////////////
         public void ConfigureServices(IServiceCollection services) {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+            services.AddHealthChecks();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        //HTTP request pipeline configuration //////////////////////////////////////////////////////
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
-            if (env.IsDevelopment()) {
-                app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
-            }
-            else {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
             app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapHealthChecks("/healthz");
+
+                endpoints.MapControllerRoute(
+                    name: "defaultAreas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+                
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                
                 endpoints.MapRazorPages();
             });
         }
