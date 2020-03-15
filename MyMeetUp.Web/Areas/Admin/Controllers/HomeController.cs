55f70025268de480357a6ebc7b6713ffc237e4c6
Marcos Rodrigo <marcosrlanuza@hotmail.com>
2020-03-15 21:32:42 +0100

    Add an admin area an a basic home controller.

---
+++ MyMeetUp.Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using MyMeetUp.Web.Configuration;
+using MyMeetUp.Web.Data;
+using MyMeetUp.Web.Models;
+
+namespace MyMeetUp.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = RolesData.Administrator)]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IOptions<AppSettings> _appSettings;
+
+        public HomeController(ILogger<HomeController> logger, IOptions<AppSettings> appSettings) {
+            _logger = logger;
+            _appSettings = appSettings;
+        }
+
+        public IActionResult Index() {
+            return View();
+        }
+
+        
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
