8e88d5039d0f130113de3f51ce88fbb1dc20062e
Marcos Rodrigo <marcosrlanuza@hotmail.com>
2020-03-28 12:45:42 +0100

    Added user index in Admin area.

---
+++ MyMeetUp.Web/Areas/Admin/Controllers/ApplicationUsersController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MyMeetUp.Web.Areas.Admin.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MyMeetUp.Web.Data;
+using MyMeetUp.Web.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace MyMeetUp.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = RolesData.Administrator)]
+    public class ApplicationUsersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private UserManager<ApplicationUser> _userManager;
+        private IPasswordHasher<ApplicationUser> _passwordHasher;
+
+        public ApplicationUsersController(ApplicationDbContext context, UserManager<ApplicationUser> userManager, IPasswordHasher<ApplicationUser> passwordHasher) {
+            _userManager = userManager;
+            _context = context;
+            _passwordHasher = passwordHasher;
+        }
+
+        public IActionResult Index() {
+            return View(FechIndexViewUserList());
+        }
+
+        private List<ApplicationUserIndexViewModel> FechIndexViewUserList() {
+            List<ApplicationUserIndexViewModel> Users = new List<ApplicationUserIndexViewModel>();
+            foreach (ApplicationUser user in _userManager.Users) {
+                Users.Add(new ApplicationUserIndexViewModel()
+                {
+                    Id = user.Id,
+                    Name = user.Name,
+                    Surname = user.Surname,
+                    Email = user.Email,
+                    LinkedIn = user.LinkedIn,
+                    DNI = user.DNI,
+                    Phone = user.Phone,
+                    City = user.City,
+                    Country = user.Country,
+                    EntryDate = user.EntryDate,
+                    LeavingDate = user.LeavingDate
+                });
+            }
+            return Users;
+        }
+    }
+}
---
+++ MyMeetUp.Web/Areas/Admin/ViewModels/ApplicationUserIndexViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyMeetUp.Web.Areas.Admin.ViewModels
+{
+    public class ApplicationUserIndexViewModel
+    {
+        public string Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string LinkedIn { get; set; }
+        [StringLength(9, MinimumLength = 9)]
+        [RegularExpression(@"[0-9]{8}[A-Z]", ErrorMessage = "The DNI format is not valid.")]
+        public string DNI { get; set; }
+        [Phone]
+        public string Phone { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public DateTime EntryDate { get; set; }
+        public DateTime? LeavingDate { get; set; }
+        
+    }
+}
