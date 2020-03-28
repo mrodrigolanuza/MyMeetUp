a3d62664f2460869ad91f700db8487f6eb248be5
Marcos Rodrigo <marcosrlanuza@hotmail.com>
2020-03-28 20:21:15 +0100

    Added carousel to landing home page.

--- MyMeetUp.Web/wwwroot/js/site.js
+++ MyMeetUp.Web/wwwroot/js/site.js
@@ -1,7 +1,17 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Añadir controles de interacción avanzada a las tablas HTML mediante "www.datatables.net"
+
 $(document).ready(function () {
+    // Añadir controles de interacción avanzada a las tablas HTML mediante "www.datatables.net"
     $('.datatable').DataTable();
+
+    // Configuración del carrusel de imágenes de la pantalla Home principal
+    $('#homeCarousel').carousel({
+        interval: 3000
+    })
+
+    $('#homeCarousel').on('slid', function () {
+        alert("Trasnsición realizada!");
+    }); 
 });
