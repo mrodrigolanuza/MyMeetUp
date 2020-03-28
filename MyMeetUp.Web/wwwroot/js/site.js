64d9c72761a76b4e251d149e3d61e6123cfa4c08
Marcos Rodrigo <marcosrlanuza@hotmail.com>
2020-03-28 14:04:24 +0100

    Added Datatables JS to Area/Admin/Role index

--- MyMeetUp.Web/wwwroot/js/site.js
+++ MyMeetUp.Web/wwwroot/js/site.js
@@ -1,4 +1,7 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+// Añadir controles de interacción avanzada a las tablas HTML mediante "www.datatables.net"
+$(document).ready(function () {
+    $('.datatable').DataTable();
+});
