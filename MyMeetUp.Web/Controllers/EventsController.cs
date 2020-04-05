40b8b34a9c4934ba5961338d76a912febb0fe83a
Marcos Rodrigo <marcosrlanuza@hotmail.com>
2020-04-05 14:50:16 +0200

    Added the possibility to a user of subscribing to a particular event if it is comming and no out of date.

--- MyMeetUp.Web/Controllers/EventsController.cs
+++ MyMeetUp.Web/Controllers/EventsController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using MyMeetUp.Web.Data;
 using MyMeetUp.Web.Models;
 
@@ -13,10 +15,13 @@ namespace MyMeetUp.Web.Controllers
     public class EventsController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ILogger _logger;
 
-        public EventsController(ApplicationDbContext context)
+        public EventsController(ApplicationDbContext context, UserManager<ApplicationUser> userManager, ILogger<EventsController> logger)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         //// GET: Events
@@ -29,18 +34,60 @@ namespace MyMeetUp.Web.Controllers
         // GET: Events/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null)
+            if (id == null) {
+                _logger.LogWarning($"Detalles del Evento {id} no encontrados");
                 return NotFound();
+            }
+                
             
             var eventSelected = await _context.Events
                 .Include(ec => ec.EventCategory)
                 .Include(g => g.Group)
                 .FirstOrDefaultAsync(m => m.Id == id);
             
-            if (eventSelected == null)
+            if (eventSelected == null) {
+                _logger.LogError($"Consulta de base de datos NOK sobre el Evento {id}. No encontrado.");
                 return NotFound();
-            
+            }
+                
             return View(eventSelected);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddNewAttendant(string userId, int eventId) 
+        {
+            ApplicationUser user = await _userManager.FindByIdAsync(userId);
+            Event eventSelected = await _context.Events.Where(e => e.Id == eventId).FirstOrDefaultAsync();
+            EventAttendanceState eventAttendaceState = await _context.EventAttendanceStates.Where(ea => ea.State == "I WILL ATTEND").FirstOrDefaultAsync();
+            if ((user is null) || (eventSelected is null))
+                return Json(new { success = false });
+
+            EventAttendance eventAttendance = new EventAttendance { 
+                ApplicationUser = user,
+                ApplicationUserId = user.Id,
+                Event = eventSelected,
+                EventId = eventSelected.Id,
+                EventAttendanceState = eventAttendaceState,
+                EventAttendanceStateId = eventAttendaceState.Id
+            };
+
+            try {
+                if (_context.EventAttendances.Where(ea => ((ea.ApplicationUserId == userId) && (ea.EventId == eventSelected.Id))).Any()) {
+                    EventAttendance actualEventAttendace = await _context.EventAttendances.Where(ea => ((ea.ApplicationUserId == userId) && (ea.EventId == eventSelected.Id))).AsNoTracking().FirstOrDefaultAsync();
+                    eventAttendance.Id = actualEventAttendace.Id;
+                    _context.EventAttendances.Update(eventAttendance);
+                }
+                else {
+                    _context.EventAttendances.Add(eventAttendance);
+                }
+                await _context.SaveChangesAsync();
+            } catch (Exception e) {
+                _logger.LogCritical($"EXCEPCIÓN: {e.Message}");
+                return Json(new { success = false });
+            }
+
+            return Json(new { success = true });
         }
 
         // GET: Events/Create
---
+++ MyMeetUp.Web/wwwroot/js/events.js
@@ -0,0 +1,25 @@
+﻿function anotarNuevoAsistenteAlEvento() {
+    console.info("Entrada en la función..");
+    $.ajax({
+        url: "/Events/AddNewAttendant",
+        type: "POST",
+        data: {
+            __RequestVerificationToken: $('input[name="__RequestVerificationToken"]',
+                $("#__EventAttendanceAntiForgeryToken")).val(),
+            userId: $("#userId").val(),
+            eventId: $("#eventId").val()
+        },
+        success: function (data) {
+            alert("Te esperamos!!");    
+            if (data !== null) {
+                
+            }
+        },
+        error: function (xhr) {
+            alert("error");
+        },
+        complete: function () {
+            alert("Completado!!");
+        }
+    });
+}
