2e1e7684d4952993a17b663aee45933cebfa5fac
Marcos Rodrigo <marcosrlanuza@hotmail.com>
2020-04-17 20:14:40 +0200

    Added basic functionality fro sending sendgrid emails to users after reading a message form event queue.

--- MyMeetUp.FunctionApp.QueueEventReceiver/QueueMessageReceiver.cs
+++ MyMeetUp.FunctionApp.QueueEventReceiver/QueueMessageReceiver.cs
@@ -1,5 +1,8 @@
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
 
 namespace MyMeetUp.FunctionApp.QueueEventReceiver
 {
@@ -7,7 +10,21 @@ namespace MyMeetUp.FunctionApp.QueueEventReceiver
     {
         [FunctionName("QueueMessageReceiver")]
         public static void Run([QueueTrigger("mymeetupeventsqueue", Connection = "AzureWebJobsStorage")]string myQueueItem, ILogger log) {
-            log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+            log.LogInformation($"C# Queue trigger function processed: {myQueueItem} >>> Sending SendGridMail to marcosrlanuza@hotmail.com");
+            SendNewsletterToUsers(myQueueItem);
+        }
+
+        private static async void SendNewsletterToUsers(string message) {
+            string apiKey = Environment.GetEnvironmentVariable("MyMeetupSendgridApiKey");
+            var client = new SendGridClient(apiKey);
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress("newsletter@fpplusplustechcommunity.com", "FP++ TechCommunity Team"),
+                Subject = message,
+                HtmlContent = "Enviado Sendgrid Mail mediante envento en Queue"
+            };
+            msg.AddTo(new EmailAddress("marcosrlanuza@hotmail.com"));
+            var response = await client.SendEmailAsync(msg);
         }
     }
 }
