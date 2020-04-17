5fc21c65ffc1f9b3c4396d46189d888737de6636
Marcos Rodrigo <marcosrlanuza@hotmail.com>
2020-04-17 20:58:39 +0200

    Added switch structure to perform different actions depending on the event type received.

---
+++ MyMeetUp.FunctionApp.QueueEventReceiver/Data/EventQueueMessages.cs
@@ -0,0 +1,12 @@
+﻿
+namespace MyMeetUp.FunctionApp.QueueEventReceiver.Data
+{
+    public class EventQueueMessages
+    {
+        public const string GROUP_CREATED = "GROUP_CREATED";
+
+        public const string EVENT_CREATED = "EVENT_CREATED";
+
+        public const string NEW_GROUP_MEMBER = "NEW_GROUP_MEMBER";
+    }
+}
--- MyMeetUp.FunctionApp.QueueEventReceiver/QueueMessageReceiver.cs
+++ MyMeetUp.FunctionApp.QueueEventReceiver/QueueMessageReceiver.cs
@@ -3,18 +3,59 @@ using Microsoft.Extensions.Logging;
 using SendGrid;
 using SendGrid.Helpers.Mail;
 using System;
+using MyMeetUp.FunctionApp.QueueEventReceiver.Data;
 
 namespace MyMeetUp.FunctionApp.QueueEventReceiver
 {
-    public static class QueueMessageReceiver
+    public class QueueMessageReceiver
     {
+        private string[] eventInformation;
+        private string eventType;
+
         [FunctionName("QueueMessageReceiver")]
-        public static void Run([QueueTrigger("mymeetupeventsqueue", Connection = "AzureWebJobsStorage")]string myQueueItem, ILogger log) {
-            log.LogInformation($"C# Queue trigger function processed: {myQueueItem} >>> Sending SendGridMail to marcosrlanuza@hotmail.com");
-            SendNewsletterToUsers(myQueueItem);
+        public void Run([QueueTrigger("mymeetupeventsqueue", Connection = "AzureWebJobsStorage")]string myQueueItem, ILogger log) {
+            try {
+                AnalyzeEventReceived(myQueueItem);
+                PerformAssociatedEventActions();
+                LogEventAsProcessedOK(myQueueItem, log);
+            } catch (Exception ex) {
+                LogEventAsProcessedWithException(myQueueItem, log, ex);
+                SendNewsletterToUsers($"Event >> {eventType} not recognized!!");
+            }
+
         }
 
-        private static async void SendNewsletterToUsers(string message) {
+        private void AnalyzeEventReceived(string myQueueItem) {
+            if (myQueueItem.Contains(":")) {
+                eventInformation = myQueueItem.Split(new char[] { ':' });
+                eventType = eventInformation[0];
+            }
+        }
+        private void PerformAssociatedEventActions() {
+            switch (eventType) {
+                case EventQueueMessages.GROUP_CREATED:
+                    SendNewsletterToUsers($"Event >> {eventType} recived!!");
+                    break;
+                case EventQueueMessages.EVENT_CREATED:
+                    SendNewsletterToUsers($"Event >> {eventType} recived!!");
+                    break;
+                case EventQueueMessages.NEW_GROUP_MEMBER:
+                    SendNewsletterToUsers($"Event >> {eventType} recived!!");
+                    break;
+                default:
+                    throw new Exception("Queue message not recognized.");
+            }
+        }
+
+        private void LogEventAsProcessedOK(string myQueueItem, ILogger log) {
+            log.LogInformation($"C# Queue trigger function processed: {myQueueItem} >>> Sending SendGridMail to marcosrlanuza@hotmail.com");
+        }
+
+        private void LogEventAsProcessedWithException(string myQueueItem, ILogger log, Exception ex) {
+            log.LogError($"EXCEPTION >> QueueMessageReceiver >> Queue message received: '{myQueueItem}' >>> Exception Message: {ex.Message}");
+        }
+
+        private async void SendNewsletterToUsers(string message) {
             string apiKey = Environment.GetEnvironmentVariable("MyMeetupSendgridApiKey");
             var client = new SendGridClient(apiKey);
             var msg = new SendGridMessage()
